e38da28f3feb9fe7bf42cb16e8f10336d3cb8903
Aaron Hellam <aaronhellam@gmail.com>
2022-05-03 10:45:05 -0700

    commit tree 4072a92610069d1a8b8fb7ee2081fc57ee27ab5e

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+
+# POST /login: run the SessionsController#create method
+  post "/login", to: "sessions#create"
+
+# DELETE /logout: run the SessionsController#destroy method
+  delete "/logout", to: "sessions#destroy"
+  
+# GET /me: run the UsersController#show method
+  get "/me", to: "users#show"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
